1a7d865c2df0af8357f7b6345f3694cf6a1c68ff
Nathan Beck <nathan.beck@fleishman.com>
2018-11-08 10:19:31 -0600

    Update lead and learning paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,9 @@ export default ({ data }) => {
   return (
     <Layout>
       <main className="Home">
-        <p className="lead">Former high school Spanish teacher, turned fullstack JavaScript engineer. Won't stop learning. Never want to stop connecting.</p>
+        <p className="lead">Former high school Spanish teacher, turned fullstack JavaScript engineer. Won't stop learning. Passionate about helping others. Love to hear other's stories.</p>
 
-        <p>As of Oct. 29, 2018, learning about MongoDB & Mongoose, password security best practices, and ES6 class syntax. Wanna learn together? Get in touch!</p>
+        <p>As of Nov. 8, 2018, learning about MongoDB & all things MySQL. Databases FTW! Wanna learn together? Get in touch!</p>
 
         <p>
           <a href="https://github.com/njosefbeck/" target="_blank" rel="noopener noreferrer">github</a> &middot; <a href="https://www.linkedin.com/in/njosefbeck" target="_blank" rel="noopener noreferrer">linkedIn</a> &middot; <a href="https://twitter.com/njosefbeck" target="_blank" rel="noopener noreferrer">twitter</a>
